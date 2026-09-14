22d2b4466755d99464b78e60c990878e65bbbdcf
Диана Шавельская <Shavelskay_DA_25@student.grsu.by>
2026-09-14 22:32:01 +0300

    Добавлено чтение текстового файла

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+using System;
+using System.IO;
+
+namespace LabWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "Podarok.txt";
+            string text = File.ReadAllText(filePath);
+            Console.WriteLine(text);
+        }
+    }
+}﻿
