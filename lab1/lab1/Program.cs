84d319563aaacc31631b24c2694215e7dfb3b43c
Диана Шавельская <Shavelskay_DA_25@student.grsu.by>
2026-09-14 22:59:41 +0300

    Добавлена проверка исходного файла

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,15 +1,22 @@
 using System;
 using System.IO;
 
-namespace LabWork
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace LabWork{
+    class Program{
+        static void Main(string[] args){
             string filePath = "Podarok.txt";
-            string text = File.ReadAllText(filePath);
+            if (!File.Exists(filePath)){
+                Console.WriteLine($"Ошибка: Файл '{filePath}' не найден.");
+                return;
+            }
+            string text = GetText(filePath);
             Console.WriteLine(text);
         }
+
+        static string GetText(string filePath){
+            using (StreamReader reader = new StreamReader(filePath, System.Text.Encoding.UTF8)){
+                return reader.ReadToEnd();
+            }
+        }
     }
-}﻿
+}
